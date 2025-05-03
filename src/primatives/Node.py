37140d8528f621d4c4a43c4734a6433becc795a5
jack-norrie <jacknorrie6785@gmail.com>
2025-05-03 22:29:32 +0100

    Change nodes to point backwards

--- src/primatives/Node.py
+++ src/primatives/Node.py
@@ -1,11 +1,12 @@
 import numpy as np
+from typing import Self
 
 
 class Node:
-    def __init__(self, value: np.ndarray, _children: None | list = None):
+    def __init__(self, value: np.ndarray, _parents: None | tuple[Self, ...] = None):
         self.value = value
         self.grad = 0
-        self._children = [] if _children is None else _children
+        self._parents = tuple() if _parents is None else _parents
 
     def __repr__(self) -> str:
         return f"Node({repr(self.value)})"
