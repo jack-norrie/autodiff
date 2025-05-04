Fix starting backprop node bug

main.py
@@ -1,18 +1,22 @@
 from src.primatives import Node
-from src.functions import add
+from src.functions import add, mul
 import numpy as np
 
 
 def main():
     a = Node(np.array(10))
     b = Node(np.array(20))
-    c = add(a, b)
+    c = mul(a, b)
 
     d = Node(np.array(50))
-    e = add(b, d)
+    e = add(c, d)
 
     e.backward()
-    print(e)
+    print(a.grad)
+    print(b.grad)
+    print(c.grad)
+    print(d.grad)
+    print(e.grad)
 
 
 if __name__ == "__main__":

src/functions.py
@@ -24,7 +24,7 @@ class Mul(Function):
 
     @staticmethod
     def backward(x: Node, y: Node) -> tuple[np.ndarray, ...]:
-        return (x.value, x.value)
+        return (y.value, x.value)
 
 
 mul = Mul()