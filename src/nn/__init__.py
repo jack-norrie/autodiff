d1b3de4ca7ef2174de96218a3a2e698b24602114
jack-norrie <jacknorrie6785@gmail.com>
2025-05-08 20:31:26 +0100

    Update nn API

--- src/nn/__init__.py
+++ src/nn/__init__.py
@@ -1,2 +1,21 @@
 from .Vector import Vector
 from .Matrix import Matrix
+from .components import Sequential, Linear, Component
+from .activation import sigmoid, tanh, relu
+from .optim import SGD, MomentumSGD, AdaGrad, RMSProp, Adam
+
+__all__ = [
+    "Vector",
+    "Matrix",
+    "Sequential",
+    "Linear",
+    "Component",
+    "sigmoid",
+    "tanh",
+    "relu",
+    "SGD",
+    "MomentumSGD",
+    "AdaGrad",
+    "RMSProp",
+    "Adam",
+]
